78d94e75f7147836fa8ed61403e3b91ec9693e85
Ricardo <radg@Mac-mini-de-Ricardo.local>
2025-11-07 21:04:04 -0600

    [Fixes and added example]

--- Sources/QudiaColorWheel/Classes/Models/Constants/ImageConstants.swift
+++ Sources/QudiaColorWheel/Classes/Models/Constants/ImageConstants.swift
@@ -6,5 +6,7 @@ internal enum Asset {
 
 internal struct ImageAsset {
     let name: String
-    var image: UIImage { .init(imageLiteralResourceName: name) }
+    var image: UIImage { .init(named: name,
+                               in: .module,
+                               with: nil) ?? .init() }
 }
